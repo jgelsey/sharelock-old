69ef6ff98c9e1d8101182a0f99b9be75ffe297ba
jgelsey <github@gelsey.com>
2020-03-31 21:17:32 -0700

    foo

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ var provider_friendly_name = {
     'yahoo': 'Yahoo'
 };
 
-console.log("domain: ",process.env.AUTH0_DOMAIN,"clientID",process.env.AUTH0_CLIENT_ID);
-console.log("done");
 
 var strategy = new Auth0Strategy({
     domain: process.env.AUTH0_DOMAIN,
@@ -37,6 +35,9 @@ var strategy = new Auth0Strategy({
     // profile has all the information from the user
     console.log("hello -- profile: ",profile);
     console.log("done");
+    console.log("domain: ",process.env.AUTH0_DOMAIN,"clientID",process.env.AUTH0_CLIENT_ID);
+	console.log("done");
+
     return done(null, profile);
 });
 
