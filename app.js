f515ee077e64bf30921c4ee7c69d6a840bec846a
jgelsey <github@gelsey.com>
2020-03-31 22:36:09 -0700

    foo

--- app.js
+++ app.js
@@ -332,8 +332,8 @@ function v1_get() {
 
 
         if (req.user && req.user.provider !== 'twitter' && !req.user._json.email_verified) {
-        	// console.log("hello -- req.user ", req.user);
-        	// console.log("done");
+        	console.log("hello -- req.user._json ", req.user._json);
+        	console.log("done");
             return res.render('invalid', { details: 'Your e-mail has not been verified xxyyyz'});
         }
 
