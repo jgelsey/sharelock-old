f00fc0d3c42a0a18522baf1cc4d2bfa46fb8e1c7
jgelsey <github@gelsey.com>
2020-04-02 10:22:41 -0700

    foo

--- app.js
+++ app.js
@@ -53,14 +53,17 @@ var options = {
   }
 };
 
-var access_token=request(options, function (error, response, body) {
+var access_token;
+
+request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
   console.log("get access token: body.access_token", body.access_token);
-  return(body.access_token)
+  access_token=body.access_token;
+  return(access_token);
 });
 
-console.log("returned body.access_token",body.access_token);
+console.log("returned access_token",access_token);
 console.log("done");
 
 var strategy = new Auth0Strategy({
