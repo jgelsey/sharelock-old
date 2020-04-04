c6b331d6de9cfb82ede80fd9ca5278df36f62a7e
jgelsey <github@gelsey.com>
2020-04-04 13:44:42 -0700

    foo

--- app.js
+++ app.js
@@ -44,13 +44,13 @@ var strategy = new Auth0Strategy({
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK,
+    callbackURL: process.env.AUTH0_CALLBACK
     // redirectUri: process.env.Auth0_CALLBACK_URL, 
     // audience: 'https://' + process.env.AUTH0_DOMAIN + '/userinfo',
     // responseType: 'code',
-    scope: 'openid',
-    scope:'profile',
-    scope:'email'
+    // scope: 'openid',
+    // scope:'profile',
+    // scope:'email'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
